1cd2626512dcbab1bc405562637f5d2ac677de4c
avi tshuva <dharmax@gmail.com>
2016-03-13 20:32:38 +0200

    typescript works

---
+++ index.ts
@@ -0,0 +1,3 @@
+require('./css/styles.css');
+require('./tsout/app.js');
+document.write('it works better');
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+
+export class MyComponent1 {
+
+  public name:string
+  constructor() {
+    document.write('meow')
+  }
+
+}
+
+new MyComponent1();
